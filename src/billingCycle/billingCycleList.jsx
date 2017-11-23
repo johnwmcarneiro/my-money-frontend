0471484c07b3aa97df971ca4173b126bc835dd87
John W. M. Carneiro <johnmarques87@gmail.com>
2017-11-23 13:21:50 -0300

    Iniciado o Redux Form; Mensagens com o Toastr

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -11,7 +11,7 @@ class BillingCycleList extends Component {
   renderRows() {
     const list = this.props.list || []
     return list.map(bc => (
-      <tr key="{bc._id}">
+      <tr key={bc._id}>
         <td>{bc.name}</td>
         <td>{bc.month}</td>
         <td>{bc.year}</td>
